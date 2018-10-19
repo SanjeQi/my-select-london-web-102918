802b1486ee1683322f06573d96f9d46b640a5720
Cristian  CC <cristi.nord@gmail.com>
2018-10-19 20:27:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,11 @@
 def my_select(array) 
-  i =0 
-  select = [] 
-  while i < array.length 
-    if (yield(array[i]))
-      # select.push(array[i])OR 
-      select << array[i] 
-    end 
-    i += 1 
+  new_arr = []
+  i = 0
+  while i < arr.length 
+      if (yield(arr[i]))
+        new_arr.push(arr[i]) 
+      end 
+        i += 1 
   end 
-  select 
+  new_arr 
 end
